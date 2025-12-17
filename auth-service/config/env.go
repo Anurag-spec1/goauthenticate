ece2800733b603a386c7c6eb655a66a447237d2a
Anurag-spec1 <anurag13360@gmail.com>
2025-12-17 23:33:37 +0530

    env issue

--- auth-service/config/env.go
+++ auth-service/config/env.go
@@ -3,13 +3,16 @@ package config
 import (
 	"log"
 	"os"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("No .env file found, using environment variables")
+		}
 	}
 }
 
